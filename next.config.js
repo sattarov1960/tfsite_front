cf5eff5d9c9a9b9ae405acc4bc401b219a15e647
sattarov1960 <89763917+sattarov1960@users.noreply.github.com>
2023-10-04 18:23:09 +0700

    deploy development

--- next.config.js
+++ next.config.js
@@ -4,9 +4,13 @@ const withNextIntl = require('next-intl/plugin')(
 );
 
 module.exports = withNextIntl({
+    publicRuntimeConfig: {
+        // Переменная окружения для определения режима разработки или продакшена
+        ENVIRONMENT: process.env.NODE_ENV === 'production' ? 'deploy' : 'development',
+    },
     env: {
-        api: "http://127.0.0.1:8000",
-        current: "http://127.0.0.1:3000",
-        localhost_api: "http://127.0.0.1:8000",
-    }
+        api: process.env.NODE_ENV === 'production' ? 'https://tf2key.ru/api' : 'http://127.0.0.1:8000',
+        current: process.env.NODE_ENV === 'production' ? 'https://tf2key.ru' : 'http://127.0.0.1:3000',
+        localhost_api: 'http://127.0.0.1:8000',
+    },
 });
