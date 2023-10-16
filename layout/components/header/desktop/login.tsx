a4143946266b64e9dda97a7a90041fe7d2a23db3
sattarov1960 <89763917+sattarov1960@users.noreply.github.com>
2023-10-16 22:43:04 +0700

    try analytik login.tsx

--- layout/components/header/desktop/login.tsx
+++ layout/components/header/desktop/login.tsx
@@ -8,17 +8,19 @@ import {useTranslations} from "next-intl";
 export const Login: FC = () => {
     const t = useTranslations()
     return (
-        <Link href={`${process.env.api}/login`}>
-            <div className={styles.login_btn}>
-                <Image
-                    src="/login_steam.svg"
-                    width={36}
-                    height={36}
-                    alt="Login"
-                />
-                <span className={styles.login_btn_text}>{t("Log in through Steam")}</span>
-            </div>
-        </Link>
+        <button>
+            <Link href={`${process.env.api}/login`}>
+                <div className={styles.login_btn}>
+                    <Image
+                        src="/login_steam.svg"
+                        width={36}
+                        height={36}
+                        alt="Login"
+                    />
+                    <span className={styles.login_btn_text}>{t("Log in through Steam")}</span>
+                </div>
+            </Link>
+        </button>
     )
 }
 
