60901cf6dc1e4adb2cc1c285d13e60d81781e14c
sattarov1960 <89763917+sattarov1960@users.noreply.github.com>
2023-12-11 19:16:09 +0700

    add refer login

--- layout/components/header/desktop/login.tsx
+++ layout/components/header/desktop/login.tsx
@@ -3,13 +3,18 @@ import Link from 'next/link'
 import styles from "@/styles/header/desktop/login.module.css"
 import Image from "next/image";
 import {useTranslations} from "next-intl";
+import { useRouter } from 'next/router';
 
 
 export const Login: FC = () => {
+    const router = useRouter();
+    console.log(router.query)
+    const rValue = router.query.r;
+    console.log(rValue)
     const t = useTranslations()
     return (
         <button>
-            <Link href={`${process.env.api}/login`}>
+            <Link href={`${process.env.api}/login${rValue ? "r=" + rValue : ""}`}>
                 <div className={styles.login_btn}>
                     <Image
                         src="/login_steam.svg"
