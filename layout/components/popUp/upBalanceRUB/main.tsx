fbc0c61b8182df2972d9b7fd201bf4fdd9c17398
sattarov1960 <89763917+sattarov1960@users.noreply.github.com>
2024-02-15 15:58:42 +0700

    update exchange

--- layout/components/popUp/upBalanceRUB/main.tsx
+++ layout/components/popUp/upBalanceRUB/main.tsx
@@ -1,21 +1,22 @@
 "use client"
 
-import {FC, useEffect} from "react";
+import {FC} from "react";
 import {useTranslations} from "next-intl";
 import styles from "@/styles/popUp/upBalanceRUB/main.module.css"
 import Image from "next/image";
 import { useStoreUpBalanceRUB } from "@/store/user";
 import {PaymentDataI} from "@/interface/paymentData";
 import axios from "axios";
-import {getCookie, setCookie} from "@/utilities/Cookies";
 import Link from "next/link";
+import {toast} from "react-toastify";
 
 const paymentData: PaymentDataI = {
     "CardAIFORY": {coms: 1.035, comsView: 3.5, min: 300, name: "Card"},
     "CardGM": {coms: 1.045, comsView: 4.5, min: 25, name: "Card"},
     "QiwiGM": {coms: 1.075, comsView: 7.5, name: "Qiwi"},
     "BtcGM": {coms: 1.02, comsView: 2, name: "BTC"},
-    "EthGM": {coms: 1.02, comsView: 2, name: "ETH"}
+    "EthGM": {coms: 1.02, comsView: 2, name: "ETH"},
+    "SBP": {coms: 1, comsView: 0, name: "SBP"}
 }
 export const Main: FC = () => {
     const t = useTranslations()
@@ -37,6 +38,9 @@ export const Main: FC = () => {
     //     return isError
     // }
     const createUp = async () => {
+        if (store.active === "SBP"){
+            return
+        }
         // const errorEmail = checkEmail()
         // if (!errorEmail && store.balance >= 100){
         if (store.balance >= 300){
@@ -53,6 +57,40 @@ export const Main: FC = () => {
                 alert("Ошибка при создании invoice")
             }
         }
+    }
+    function copyToClipboard(text: string) {
+        if (navigator.clipboard) {
+            // Use Clipboard API if it's available
+            navigator.clipboard.writeText(text).then(function() {
+                console.log('Copying to clipboard was successful!');
+            }, function(err) {
+                console.error('Could not copy text: ', err);
+            });
+        } else {
+            let textarea = document.createElement('textarea');
+            textarea.textContent = text;
+            document.body.appendChild(textarea);
+            textarea.select();
+            try {
+                document.execCommand('copy');
+                console.log('Copying to clipboard was successful!');
+            } catch (err) {
+                console.error('Could not copy text: ', err);
+            } finally {
+                document.body.removeChild(textarea);
+            }
+        }
+        toast.success("Скопировал в буффер обмена", {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            isLoading: false,
+        });
     }
     return (
         <div className={styles.popUp_replenishmentOfTheBalance}>
@@ -167,6 +205,19 @@ export const Main: FC = () => {
                                 </div>
                                 <span className={styles.popUp_sub_main_card_percent}>2%</span>
                             </div>
+                            <div tabIndex={7} onKeyPress={(e) => e.key === 'Enter' ? store.setActive("SBP") : null} onClick={() => store.setActive("SBP")} className={`${styles.popUp_sub_main_card} ${store.active === "SBP" ? styles.popUp_sub_main_card_active : null}`}>
+                                <Image
+                                    src="/sbp_icon.svg"
+                                    width={34}
+                                    height={34}
+                                    alt="SBP"
+                                    className={styles.popUp_sub_main_card_nextIcon_sbp}
+                                />
+                                <div className={`${styles.popUp_sub_main_sub_card} ${styles.popUp_sub_main_sub_nextCard}`}>
+                                    <p className={styles.popUp_sub_main_card_mainText}>{t("Manual replenishment")}</p>
+                                </div>
+                                <span className={styles.popUp_sub_main_card_percent_red}>0%</span>
+                            </div>
                         </div>
                         <div className={styles.popUp_sub_main_cardsMobile}>
                             <div onClick={() => store.setActive("CardAIFORY")} className={`${styles.popUp_sub_main_card} ${store.active === "CardAIFORY" ? styles.popUp_sub_main_card_active : null}`}>
@@ -241,6 +292,19 @@ export const Main: FC = () => {
                                 </div>
                                 <span className={styles.popUp_sub_main_card_percent}>2%</span>
                             </div>
+                            <div onClick={() => store.setActive("SBP")} className={`${styles.popUp_sub_main_card} ${store.active === "SBP" ? styles.popUp_sub_main_card_active : null}`}>
+                                <Image
+                                    src="/sbp_icon.svg"
+                                    width={34}
+                                    height={34}
+                                    alt="SBP"
+                                    className={styles.popUp_sub_main_card_nextIcon_sbp}
+                                />
+                                <div className={`${styles.popUp_sub_main_sub_card} ${styles.popUp_sub_main_sub_nextCard}`}>
+                                    <p className={styles.popUp_sub_main_card_mainText}>{t("Manual replenishment")}</p>
+                                </div>
+                                <span className={styles.popUp_sub_main_card_percent_red}>0%</span>
+                            </div>
                         </div>
                         {/*<div className={styles.popUp_sub_main_inputBlock}>*/}
                         {/*    <span className={styles.popUp_sub_main_subText}>{t("Enter contact details")}</span>*/}
@@ -255,7 +319,8 @@ export const Main: FC = () => {
                         {/*    />*/}
                         {/*    {store.errorEmail ? <span className={styles.popUp_sub_main_warningText}>{t("Email mail is incorrect")}</span> : null}*/}
                         {/*</div>*/}
-                        <div className={`${styles.popUp_sub_main_footer} ${["CardGM", "QiwiGM", "BtcGM", "EthGM"].includes(store.active) ? styles.popUp_sub_main_footer_mini : null}`}>
+                        {store.active !== "SBP" ? <div
+                            className={`${styles.popUp_sub_main_footer} ${["CardGM", "QiwiGM", "BtcGM", "EthGM"].includes(store.active) ? styles.popUp_sub_main_footer_mini : null}`}>
                             <div className={styles.popUp_sub_main_footer_frstItem}>
                                 <span className={styles.popUp_sub_main_footer_item_text}>{t("Payment method:")}</span>
                                 <p className={styles.popUp_sub_main_footer_item_textBold}>{paymentData[store.active].name}</p>
@@ -269,22 +334,32 @@ export const Main: FC = () => {
                                     <span className={styles.popUp_sub_main_footer_item_text}>{t("Your payment:")}</span>
                                     <p className={styles.popUp_sub_main_footer_item_textBoldOrange}>
                                         ₽
-                                        <span className={`${styles.popUp_sub_main_footer_item_textBoldOrange} ${styles.popUp_sub_main_footer_item_textBoldOrangeCurr}`}>{Math.ceil(store.balance * paymentData[store.active].coms)}</span>
+                                        <span
+                                            className={`${styles.popUp_sub_main_footer_item_textBoldOrange} ${styles.popUp_sub_main_footer_item_textBoldOrangeCurr}`}>{Math.ceil(store.balance * paymentData[store.active].coms)}</span>
                                     </p>
                                 </div>
                             }
-                            <button tabIndex={8} className={styles.popUp_sub_main_footer_button} onClick={() => createUp()}>{t("Top up")}</button>
+                            <button tabIndex={8} className={styles.popUp_sub_main_footer_button}
+                                    onClick={() => createUp()}>{t("Top up")}</button>
 
-                        </div>
+                        </div> :
+                        <div className={styles.popUp_sub_main_footer_withdraw}>
+                            <p className={styles.popUp_sub_main_footer_withdraw_text} style={{color: "#FFFFFF80"}}>Вы можете пополнить баланс бота обычным переводом по<br/>СБП на номер выданной поддержкой.<br/>На данный момент минимальная сумма пополнения <span style={{color: "#FFFFFFFF"}}>1480 Р</span>.<br/><br/>Для
+                                получения реквизитов для перевода напишите на наш аккаунт поддержки: <Link target="_blank" href={"https://t.me/MannCoSupplyCrateKey"} style={{color: "#FF7610"}}>@MannCoSupplyCrateKey</Link><br/><br/>Шаблон: <span style={{color: "#FFFFFF", cursor: "pointer"}} onClick={() =>
+                                    copyToClipboard("Привет, хочу пополнить баланс tf2key.ru на ")
+                                }>Привет, хочу пополнить баланс tf2key.ru</span></p>
+                        </div>}
                         <div className={styles.popUp_sub_main_privacyPolicy}>
-                        <span className={styles.popUp_sub_main_privacyPolicy_textGray}>{t("By clicking \"Top up\", I agree to the")}<br/>
-                            <Link href={`${process.env.current}/privacy`}>
-                                <span className={styles.popUp_sub_main_privacyPolicy_textWhite}>{t("refund policy")}</span>
-                            </Link>
-                        </span>
+                            <span
+                                className={styles.popUp_sub_main_privacyPolicy_textGray}>{t("By clicking \"Top up\", I agree to the")}<br/>
+                                <Link href={`${process.env.current}/privacy`}>
+                                    <span
+                                        className={styles.popUp_sub_main_privacyPolicy_textWhite}>{t("refund policy")}</span>
+                                </Link>
+                            </span>
                         </div>
                     </div>
-                </div>
+            </div>
         </div>
     )
 }
